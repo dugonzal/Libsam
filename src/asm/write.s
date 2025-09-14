e718a4bf27295d4301ad2e12c77397056584f7f8
dugonzal <dugonzal@protonmail.com>
2025-09-14 08:43:47 +0000

    feat: compriendo la estructura del lenguaje y sus estamentos

--- src/asm/write.s
+++ src/asm/write.s
@@ -1,14 +1,31 @@
 BITS 64 ; para que compile en 64 bits
+
+SECTION .data
+    msg db "", 20, 10
+    len equ $ - msg
+
 SECTION .text
-;buffer: resb 64 ;
 GLOBAL _start ; definimos el metodo de inicio, start es main
 
 
-_start:
-    mov rax, 60 ; syscall int exit(42)
-    xor rdi, 10
+ft_write:
+    mov rax, 0 ; syscall int read(fd, char *, len)
+    mov rdi, 0
+    mov rsi, msg
+    mov rdx, len
     syscall
-   ; ret
 
 
-SECTION .data
+    mov rax, 1 ; int write(fd, msg, len)
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
+    syscall
+
+    mov rax, 60 ;exit(rdi)
+    xor rdi, rdi
+    syscall
+
+
+_start:
+    call ft_write
